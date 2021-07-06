6f64440992a254dba246a48d0cdbb42935daff80
zhangakai <1033374069@qq.com>
2021-07-06 22:42:58 +0800

    update many

--- src/main/java/CCKS/GsonToText.java
+++ src/main/java/CCKS/GsonToText.java
@@ -1,12 +1,22 @@
 package CCKS;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 public class GsonToText {
     private static final String JSON_FILE_PATH = "";
 
     public static void main(String[] args) {
-        Executors.newCachedThreadPool()
+        Executors.newCachedThreadPool();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for (int i : list) {
+
+            System.out.println(i);
+        }
     }
 
 
